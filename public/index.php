91b5abb858fe895567e49c226019631e838166e3
Livni <llmc0383@gmail.com>
2023-08-02 10:42:38 -0600

    se actualizaron rutas

--- public/index.php
+++ public/index.php
@@ -4,12 +4,15 @@ require_once __DIR__ . '/../includes/app.php';
 
 use MVC\Router;
 use Controllers\AppController;
+use Controllers\ProductoController;
+
 $router = new Router();
 $router->setBaseURL('/' . $_ENV['APP_NAME']);
 
 $router->get('/', [AppController::class,'index']);
-$router->get('productos');
-
+$router->get('/productos', [ProductoController::class,'index'] );
+$router->post('/API/productos/guardar', [ProductoController::class,'guardarAPI'] );
+$router->get('/API/productos/buscar', [ProductoController::class,'buscarAPI'] );
 
 // Comprueba y valida las rutas, que existan y les asigna las funciones del Controlador
 $router->comprobarRutas();
